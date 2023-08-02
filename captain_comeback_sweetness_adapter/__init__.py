831bf67d25fad76975ddf806b02dd5411d2d63c6
Ann Guilinger <ann.guilinger@aptible.com>
2023-08-02 08:39:27 -0400

    Send captain comeback restart metrics to configurable influxdb (#2)

--- captain_comeback_sweetness_adapter/__init__.py
+++ captain_comeback_sweetness_adapter/__init__.py
@@ -6,6 +6,7 @@ import binascii
 import logging
 
 import redis
+import influxdb
 
 
 logger = logging.getLogger('captain_comeback_sweetness_adapter')
@@ -15,6 +16,16 @@ SIDEKIQ_QUEUE = os.environ["SIDEKIQ_QUEUE"]
 
 POOL = redis.BlockingConnectionPool.from_url(
     REDIS_URL, decode_components=True, max_connections=1,
+)
+INFLUX_CLIENT = influxdb.InfluxDBClient(
+    host=os.getenv('INFLUXDB_HOST'),
+    port=os.getenv('INFLUXDB_PORT'),
+    database=os.getenv('INFLUXDB_DATABASE'),
+    username=os.getenv('INFLUXDB_USER'),
+    password=os.getenv('INFLUXDB_PASSWORD'),
+    ssl=os.getenv("INFLUXDB_SSL", 'False').lower() in ('true', '1', 't'),
+    verify_ssl=os.getenv("INFLUXDB_SSL", 'False').lower() in ('true', '1', 't'),
+    timeout=30
 )
 
 
@@ -39,6 +50,30 @@ def restart(cg, retried=False):
         restart(cg, True)
     else:
         logger.info("%s: enqueued restart: %s", cg.name(), jid)
+        
+    try:
+        host = os.environ['INFLUXDB_HOST']
+        port = os.environ['INFLUXDB_PORT']
+        db = os.environ['INFLUXDB_DATABASE']
+        logger.info("Writing metrics to %s:%s/%s", host, port, db)
+
+        INFLUX_CLIENT.write_points([
+            {
+                'measurement': 'captain_comeback_restarts',
+                'tags': {
+                    'container_id': cg.name(),
+                    'queue': SIDEKIQ_QUEUE,
+                },
+                'fields': {
+                    'retried': 1,
+                    'job_id': jid
+                }
+            }
+        ])
+    except (influxdb.exceptions.InfluxDBClientError, influxdb.exceptions.InfluxDBServerError) as e:
+        logger.warning(e)
+        logger.warning("Error writing to captain comeback restart metrics: JID %s; CID %s", jid, cg.name())
+        
 
 
 __author__ = "Thomas Orozco"
